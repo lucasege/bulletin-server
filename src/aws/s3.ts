33349236ea0a0792fb25013745bc1a2b75c3d43a
Lucas Ege <lucasege@cs.stanford.edu>
2020-07-03 12:19:31 -0700

    Adding image hosting

---
+++ src/aws/s3.ts
@@ -0,0 +1,22 @@
+import * as AWS from 'aws-sdk';
+import * as uuid from 'node-uuid';
+
+var s3 = new AWS.S3();
+
+var bucketName = 'bulletin-news-images';
+var keyPrefix = 'user_image_upload_';
+
+export const UploadImageToS3 = (imageContent: any) => {
+  var params = {
+    Bucket: bucketName,
+    Key: keyPrefix + uuid.v4(),
+    Body: imageContent
+  };
+  s3.upload(params, function(err: any, data: any) {
+    if (err) {
+      throw err;
+    }
+    console.log(`File uploaded successfully. ${data.Location}`)
+    return data.Location;
+  });
+}
--- src/server.ts
+++ src/server.ts
@@ -1,35 +1,59 @@
 import * as express from "express";
+import * as bodyParser from "body-parser";
 import { connect } from "./database/database";
 import { PostModel } from "./database/posts/posts.model";
 import { UserModel } from "./database/users/users.model";
+import { UploadImageToS3 } from "./aws/s3";
+const fs = require('fs');
 
 // TODO(lucas): enforce types
 
 const app = express();
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json({ limit: '15MB' }))
+
 app.get('/getPosts', async (req, res) => {
   PostModel.findAll()
     .then(items => res.json(items))
-})
+});
 
 app.get('/getUser/:appleUserString', async (req, res) => {
   UserModel.findUser(req.params.appleUserString)
     .then(user => res.json(user))
-})
+});
 
 app.post('/submitPost', (req, res) => {
   console.log(req.body);
   PostModel.insertMany(req.body)
     .then(() => res.json({ success: true }))
     .catch(err => res.status(404).json({ success: false }));
-})
+});
 
 app.post('/submitUser', (req, res) => {
   console.log(req.body);
   UserModel.insertMany(req.body)
     .then(() => res.json({ success: true }))
     .catch(err => res.status(404).json({ success: false }));
-})
+});
+
+app.post('/submitImage', (req, res) => {
+  try {
+    console.log("req.body ", req.body.imageSource)
+    const locator = UploadImageToS3(req.body.imageSource);
+    fs.writeFile('./out.png', req.body.imageSource, 'base64', (err) => {
+      if (err) throw err
+    });
+    res.json({
+      success: true,
+      locator: locator,
+    });
+  } catch (err) {
+    res.status(404).json({
+      success: false
+    });
+  }
+});
+
 const port = 5002;
 
 connect();
